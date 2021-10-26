aa7ceecc037ffc30d3fbeb6265c7aa2b5002dc98
Rajab Legnain <11955043+rlegnain@users.noreply.github.com>
2021-10-26 13:02:20 -0400

    Update classes.py
    Refactors the code

--- src/myPackage/classes.py
+++ src/myPackage/classes.py
@@ -2,22 +2,43 @@
 
 class Product:
     def __init__(self, product_name, manufacturer, family, model, announced_date):
-        self.product_name = product_name 
-        self.manufacturer = manufacturer    #.replace(' ','')
+        """
+        Parameter:
+        ----------
+            title:          String, A unique id for the product
+            manufacturer:   String
+            family:         String. optional grouping of products
+            model:          String
+            announced-date: String. ISO-8601 formatted date string, e.g. 2011-04-28T19:00:00.000-05:00
+        """
+        self.product_name = product_name
+        self.manufacturer = manufacturer
         self.family = family
+        self.announced_date = announced_date
         self.model = model
-        self.announced_date = announced_date        
-        
+
+
 class Listing:
     def __init__(self, title, manufacturer, currency, price):
-        self.title = title    # This line is used to replace symbols '-' or '_' with whitespace #String// description of product for sale
+        """
+        Parameters:
+        -----------
+            title:        String. Description of product for sale
+            manufacturer: String. who manufactures the product for sale
+            currency:     String. currency code, e.g. USD, CAD, GBP, etc.
+            price:        String. price, e.g. 19.99, 100.00
+        """
+        self.title = title
         self.manufacturer = manufacturer
         self.currency = currency         
         self.price = price              
 
+
 class Result:
-#A file full of Result objects is what your solution will be generating. 
-#A Result simply associates a Product with a list of matching Listing objects.
+    """
+    A file full of Result objects is what your solution will be generating.
+    A Result simply associates a Product with a list of matching Listing objects.
+    """
     def __init__(self, product_name, listings):
-        self.product_name = product_name     #String   
-        self.listings = listings             #Array[Listing]
+        self.product_name = product_name
+        self.listings = listings
